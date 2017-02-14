bea0071957abe71838cea3d85decda8ab5a1386b
Colin Bloom and Ryan McAlpin <ryan@>
2017-02-13 16:31:19 -0800

    create javascript for code

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+var userInputSentence = prompt("Enter a sentence");
+var firstAndLast = function(words) {
+return (words.charAt(0) + words.charAt(words.length - 1)).toUpperCase();
+}
+var firstLast = firstAndLast(userInputSentence);
+
+//alert(firstAndLast(userInputSentence));
+
+var lastAndFirst = function(letters) {
+return firstLast.charAt(1) + firstLast.charAt(0);
+}
+
+//alert(lastAndFirst(firstLast));
+
+var doAll = function() {
+return userInputSentence + lastAndFirst(firstLast);
+}
+//alert(doAll());
+
+var fourth = function() {
+return userInputSentence.charAt(userInputSentence.length / 2) + doAll();
+}
+var finalThing = fourth();
+//alert(fourth());
+
+var reverse = function() {
+	return finalThing.split("").reverse().join("");
+}
+alert(reverse());
